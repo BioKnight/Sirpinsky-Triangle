b3051dfb89b8459cff18acad916cd04eddafaf04
BioKnight <randycmiller1981@gmail.com>
2023-02-18 21:35:30 -0500

    Working Model

--- Sirpinsky Triangle/Form1.vb
+++ Sirpinsky Triangle/Form1.vb
@@ -1,16 +1,23 @@
-﻿Public Class frm_Main
-    Private basePoints As New ArrayList
+﻿Imports System.IO
+Imports System.Reflection.Emit
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class frm_Main
+    'Private basePoints As New ArrayList
     Private points As New ArrayList
     Private dotSize As Integer = 3
+    Private frmsize As Size
+    Private CancelToken As New Threading.CancellationToken
+    Private PointBuilder As New RandomPoints(points, CancelToken)
+    Private waiting As Boolean = False
 
     Private Sub frm_Main_Load(sender As Object, e As EventArgs) Handles Me.Load
-        If basePoints.Count = 0 Then
-            basePoints.Add(New Point(Me.Width / 2, 1))
-            basePoints.Add(New Point(1, Me.ClientSize.Height - (1 + dotSize)))
-            basePoints.Add(New Point(Me.ClientSize.Width - (1 + dotSize), Me.ClientSize.Height - (1 + dotSize)))
+        If points.Count = 0 Then
+            points.Add(New Point(Me.Width / 2, 1))
+            points.Add(New Point(1, Me.ClientSize.Height - (1 + dotSize)))
+            points.Add(New Point(Me.ClientSize.Width - (1 + dotSize), Me.ClientSize.Height - (1 + dotSize)))
         End If
         Me.Invalidate()
-
     End Sub
 
     Private Sub frm_Main_Click(sender As Object, e As MouseEventArgs) Handles Me.Click
@@ -22,8 +29,6 @@
         'Me.Invalidate()
     End Sub
 
-
-
     Private Sub draw_Point(location As Point)
         Dim Gfx As Graphics = Me.CreateGraphics
 
@@ -32,33 +37,73 @@
         GPen = New Pen(Drawing.Color.Black, 3)
         GPen.Brush = New SolidBrush(Color.Black)
 
-        'Gfx.DrawEllipse(GPen, location.X, location.Y, 1, 1)
         Gfx.FillEllipse(GPen.Brush, location.X, location.Y, 3, 3)
     End Sub
 
     Private Sub frm_Main_Paint(sender As Object, e As PaintEventArgs) Handles Me.Paint
-        For Each pnt In basePoints
-            draw_Point(pnt)
-        Next
-        For Each pnt In points
-            draw_Point(pnt)
-        Next
+        'For Each pnt In points
+        Dim x As Integer
+        While x < points.Count
+            draw_Point(points(x))
+            x += 1
+        End While
     End Sub
 
     Private Sub frm_Main_Resize(sender As Object, e As EventArgs) Handles Me.ResizeEnd
+        If points.Count > 3 Then Exit Sub
         Me.Width = Me.Height - (Me.Height / 128)
 
-        If basePoints.Count = 0 Then
-            basePoints.Add(New Point(Me.Width / 2, 1))
-            basePoints.Add(New Point(1, Me.ClientSize.Height - (1 + dotSize)))
-            basePoints.Add(New Point(Me.ClientSize.Width - (1 + dotSize), Me.ClientSize.Height - (1 + dotSize)))
+        If points.Count = 0 Then
+            points.Add(New Point(Me.Width / 2, 1))
+            points.Add(New Point(1, Me.ClientSize.Height - (1 + dotSize)))
+            points.Add(New Point(Me.ClientSize.Width - (1 + dotSize), Me.ClientSize.Height - (1 + dotSize)))
         Else
-            basePoints.Clear()
-            basePoints.Add(New Point(Me.Width / 2, 1))
-            basePoints.Add(New Point(1, Me.ClientSize.Height - (1 + dotSize)))
-            basePoints.Add(New Point(Me.ClientSize.Width - (1 + dotSize), Me.ClientSize.Height - (1 + dotSize)))
+            points.Clear()
+            points.Add(New Point(Me.Width / 2, 1))
+            points.Add(New Point(1, Me.ClientSize.Height - (1 + dotSize)))
+            points.Add(New Point(Me.ClientSize.Width - (1 + dotSize), Me.ClientSize.Height - (1 + dotSize)))
         End If
 
         Me.Invalidate()
     End Sub
+
+    Private Sub frm_Main_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Me.KeyPress
+        If e.KeyChar = Char.Parse("q") Then
+            PointBuilder.RequestStop()
+        ElseIf e.KeyChar = Char.Parse("s") And (PointBuilder.ClassThreadState = system.Threading.ThreadState.Stopped Or PointBuilder.ClassThreadState = system.Threading.ThreadState.Unstarted) Then
+            frmsize = Me.Size
+            Me.FormBorderStyle = FormBorderStyle.Fixed3D
+            Me.MinimizeBox = False
+            Me.MaximizeBox = False
+            Me.ControlBox = False
+            AddHandler PointBuilder.ThreadStateChange, AddressOf threadstatechange
+            AddHandler PointBuilder.waiting, AddressOf setwait
+            AddHandler PointBuilder.NewPoint, AddressOf NewPoint
+            PointBuilder.Begin()
+        ElseIf e.keychar = Char.Parse("i") Then
+            MsgBox(PointBuilder.count)
+        ElseIf e.KeyChar = Char.Parse("d") Then
+            PointBuilder.Pause = True
+            While (PointBuilder.Pause And Not waiting)
+
+            End While
+
+            Me.Invalidate()
+            PointBuilder.Pause = False
+        End If
+    End Sub
+
+    Private Sub threadstatechange(state As System.Threading.ThreadState)
+        MsgBox(state.ToString)
+    End Sub
+
+    Private Sub NewPoint(pnt As Point)
+        points.Add(pnt)
+        'Me.Invalidate()
+    End Sub
+
+    Private Sub setwait(wait As Boolean)
+        waiting = wait
+    End Sub
+
 End Class
---
+++ Sirpinsky Triangle/RandomPoints.vb
@@ -0,0 +1,112 @@
+﻿Imports System.Threading
+Public Class RandomPoints
+    Private token As CancellationToken
+    Private points As New ArrayList
+    Private PointCount As Integer
+    Private PointProgress As Progress(Of Integer)
+    Private ClassThread As New Thread(AddressOf BuildTriangle)
+    Public Event ThreadStateChange(State As ThreadState)
+    Public Event NewPoint(pnt As Point)
+    Public Event waiting(wait As Boolean)
+    Private wait As Boolean = False
+
+    Public ReadOnly Property ClassThreadState As ThreadState
+        Get
+            Return ClassThread.ThreadState
+        End Get
+    End Property
+    Public ReadOnly Property count As Integer
+        Get
+            Return PointCount
+        End Get
+    End Property
+
+    Public Property Pause As Boolean
+        Get
+            Return wait
+        End Get
+        Set(value As Boolean)
+            wait = value
+        End Set
+    End Property
+
+    Sub New(StartPoints As ArrayList, ByRef CancelToken As CancellationToken)
+        token = CancelToken
+        Me.points = StartPoints
+        RaiseEvent ThreadStateChange(ThreadState.Unstarted)
+    End Sub
+
+    Private Sub BuildTriangle()
+        If points.Count < 3 Then Exit Sub
+
+        PointProgress = New Progress(Of Integer)(AddressOf ReportProgress)
+
+        Dim a As Point
+        Dim b As Point
+        Thread.Sleep(100)
+        While Not token.IsCancellationRequested
+            Dim newpt As New Point()
+
+            If points.Count < 5 Then
+                a = points(GetRandom(0, 3))
+                Do
+                    b = points(GetRandom(0, points.Count))
+                Loop While a = b
+                newpt.X = (a.X + b.X) / 2
+                newpt.Y = (a.Y + b.Y) / 2
+            Else
+                a = points(GetRandom(0, 3))
+                Do
+                    b = points(GetRandom(3, points.Count))
+                Loop While a = b
+                newpt.X = (a.X + b.X) / 2
+                newpt.Y = (a.Y + b.Y) / 2
+            End If
+            Thread.Sleep(5)
+
+            points.Add(newpt)
+
+            TryCast(PointProgress, IProgress(Of Integer)).Report(points.Count)
+            RaiseEvent NewPoint(newpt)
+            While wait
+                RaiseEvent waiting(True)
+                Thread.Sleep(1000)
+            End While
+            RaiseEvent waiting(False)
+        End While
+        Thread.Sleep(1000)
+        RaiseEvent ThreadStateChange(ThreadState.Stopped)
+    End Sub
+
+    Private Sub ReportProgress(ByVal count As Integer)
+        PointCount = count
+    End Sub
+
+    Public Function GetRandom(ByVal Min As Integer, ByVal Max As Integer) As Integer
+        ' by making Generator static, we preserve the same instance '
+        ' (i.e., do not create new instances with the same seed over and over) '
+        ' between calls '
+        Static Generator As System.Random = New System.Random()
+        Return Generator.Next(Min, Max)
+    End Function
+
+    Public Sub Begin()
+        Try
+            ClassThread.Start()
+
+            RaiseEvent ThreadStateChange(ThreadState.Running)
+        Catch ex As Exception
+            RaiseEvent ThreadStateChange(ThreadState.Unstarted)
+        End Try
+    End Sub
+
+    Public Sub RequestStop()
+        Try
+            token = New CancellationToken(True)
+            RaiseEvent ThreadStateChange(ThreadState.StopRequested)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+End Class
